957b68de97a622faceeb21d07c1d5a3dee977e26
jdcarlson07 <56281452+jdcarlson07@users.noreply.github.com>
2019-10-14 19:45:38 -0500

    homework

--- src/Driver.java
+++ src/Driver.java
@@ -4,17 +4,15 @@ public class Driver
 	public static void main(String[] args) 
 	{
 		Investment a = new Investment();
-		Investment b = new Investment();
 		a.addContribution(new Contribution(1200, 1));
-		int month = 1;
-		for(int i = 0; i < 12; i++)
-		{
-			b.addContribution(new Contribution(100, month));
-		    month = month + 12;
-		}
+		a.addContribution(new Contribution(1300, 5));
+		a.addContribution(new Contribution(1400, 10));
+
+
 		
-		int totalInvestmentTime = 156;
+		int totalInvestmentTime = 12;
 		System.out.println(a.getCurrentValue(totalInvestmentTime));
-		System.out.println(b.getCurrentValue(totalInvestmentTime));
+		System.out.println(a.getTotalContributions());
+
 	}
 }
--- src/Investment.java
+++ src/Investment.java
@@ -14,17 +14,51 @@ public class Investment
 		this.theContributions.add(c);
 	}
 	
-	double getCurrentValue(int afterHowManyMonth)
+	double getCurrentValue(int afterHowManyMonths)
 	{
+		double value = 0;
+		int size = this.theContributions.size() - 1;
+		for(int i = 0; i < this.theContributions.size(); ++i)
+			{
+				
+				Contribution currCont = this.theContributions.get(i);
+				value = value + currCont.amount;
+				if(i < size)
+				{		
+					Contribution nextCont = this.theContributions.get(i + 1);
+					for(int j = currCont.monthNumber; j < nextCont.monthNumber; ++j)
+					{
+						value = value * 1.067;
+					}
+				}
+				else
+				{
+					for(int j = currCont.monthNumber; j <= afterHowManyMonths; ++j)
+					{
+						value = value * 1.067;
+					}
+				}
+				
+				
+			}
+		
+		return value;
 		//return the current value of this investment taking
 		//time and an average 8% yearly or 0.67% monthly return
 		//over afterHowManyMonths number of months
 	}
 	
+	
+	
 	double getTotalContributions()
 	{
-		//return the total amount ever investment not taking into
-		//account interest.
+		double total = 0;
+		for(int i = 0; i < this.theContributions.size(); ++i)
+		{
+			Contribution cont = this.theContributions.get(i);
+			total = total + cont.amount;
+		}
+		return total;
 	}
 	
 }
